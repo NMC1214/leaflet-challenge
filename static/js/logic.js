8812c57b76b148eccd16ae29b903789c9930b07a
Nicole <nclotus11@gmail.com>
2021-07-16 12:52:28 -0400

    commit -m "markers and circles added"

--- static/js/logic.js
+++ static/js/logic.js
@@ -16,25 +16,46 @@ L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
 
 var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 
+
 d3.json(url).then(function (response) {
     // console.log(response);
-
-    var features = response.features;
-    console.log(features)
-
     var locations = [];
     var magnitudes = [];
     var depths = [];
     var colors = [];
     var places = [];
 
+    var features = response.features;
+    // console.log(features)
+
     for (var i = 0; i < features.length; i++) {
         var properties = features[i].properties;
         // console.log(properties)
-                
+
+        magnitudes.push(properties.mag);
+        places.push(properties.place);
+
+        var geometry = features[i].geometry;
+        // console.log(geometry)
+        locations.push([geometry.coordinates[1],geometry.coordinates[0]]);
+        depths.push(geometry.coordinates[2]);
+                       
     }
 
-    
 
+    // console.log(magnitudes)
+    // console.log(places)
+    // console.log(locations)
+
+    for (var i = 0; i < locations.length; i++) {
+        L.circleMarker(locations[i], {
+        fillOpacity: 0.75,
+        color: "red",
+        fillColor: "red",
+        // Adjust radius
+        radius: magnitudes[i] * 7
+      }).bindPopup("<h2>" + places[i] + "</h2> <hr> <h3>Magnitude: " + magnitudes[i] + "</h3><h3>Depth: " + depths[i] + "</h3>").addTo(myMap);
+    }
 
 });
+
