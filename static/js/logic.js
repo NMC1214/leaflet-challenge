c38bfa872c41158689c3af9d59f86fb93de72fd0
Nicole <nclotus11@gmail.com>
2021-07-16 11:49:40 -0400

    commit -m "working into features array"

--- static/js/logic.js
+++ static/js/logic.js
@@ -2,22 +2,39 @@
 var myMap = L.map("map", {
     center: [37.09, -95.71],
     zoom: 5
-  });
-  
-  // Add a tile layer
-  L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+});
+
+// Add a tile layer
+L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
     tileSize: 512,
     maxZoom: 18,
     zoomOffset: -1,
     id: "mapbox/streets-v11",
     accessToken: API_KEY
-  }).addTo(myMap);
+}).addTo(myMap);
 
-  var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 
-  d3.json(url).then(function(response) {
-      console.log(response);
+d3.json(url).then(function (response) {
+    // console.log(response);
+
+    var features = response.features;
+    console.log(features)
+
+    var locations = [];
+    var magnitudes = [];
+    var depths = [];
+    var colors = [];
+    var places = [];
+
+    for (var i = 0; i < features.length; i++) {
+        var properties = features[i].properties;
+        // console.log(properties)
+                
+    }
+
+    
 
 
-  });
+});
